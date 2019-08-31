9aa8e0c89cc8f6a32b04362e7bbae4661b62b6ba
jalalabdul <jalalabdulaziz@gmail.com>
2019-08-31 19:26:11 +0800

    regular update

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,36 +10,38 @@ document.addEventListener("DOMContentLoaded", function () {
         .all([this.newContainerLoading, this.fadeOut()])
         .then(this.fadeIn.bind(this));
     },
-  
+
     fadeOut: function () {
       $(this.oldContainer).removeClass("anim-in");
       $(this.oldContainer).fadeOut(200);
+      $("footer").fadeOut(200);
 
       $("html, body").stop(true, false).animate({
         scrollTop: 0
       }, 600);
 
-      return new Promise(function(resolve, reject) {
-        window.setTimeout(function() {
+      return new Promise(function (resolve, reject) {
+        window.setTimeout(function () {
           resolve();
         }, 400);
       });
     },
-  
-    fadeIn: function() {
+
+    fadeIn: function () {
+      $("footer").fadeIn(200);
       $(this.newContainer).addClass("anim-in");
       this.done();
     }
   });
-  
+
   Barba.Pjax.getTransition = function () {
     return FadeTransition;
   };
-  
+
   Barba.Pjax.start();
 });
 
-Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container, rawHTML) {
+Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container, rawHTML) {
   window.lazySizes.init();
   $('.top-button').on('click', function () {
     $("html, body").stop(true, false).animate({
