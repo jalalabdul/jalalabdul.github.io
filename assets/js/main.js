cb07a891717727476523ad64f11ec8d7db01b305
Jalal Abdul Aziz <jalalabdulaziz@gmail.com>
2021-03-17 19:03:29 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,7 +141,20 @@ Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, containe
   headerAnimation();
   mobileToggle();
 
-  const player = new Plyr('#player');
+  $('.video-popup').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-anim',
+    removalDelay: 500,
+    preloader: false,
+    fixedContentPos: false,
+    iframe: {
+      markup: '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        '</div>',
+      srcAction: 'iframe_src',
+    }
+  });
 
   if ($(window).width() > 769) {
     // Screensaver
